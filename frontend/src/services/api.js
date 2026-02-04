d4589796cbf558b8a0abf12dc0f797018fbb1f1e
smilmduo <mishraaman8115@gmail.com>
2026-02-04 08:39:50 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'http://127.0.0.1:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default api;
+
 
 
